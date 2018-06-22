1cefabd2b34d1a7d2857938318c3a69368b640a0
Luis Gonzalez <luisarmando1234@gmail.com>
2018-06-22 12:59:42 -0500

    Update environment.ts

--- backend/src/environments/environment.ts
+++ backend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiHost: '//localhost/api/v1',
+  apiHost: '//35.193.210.198/api/v1',
   tokenName: 'backend-token',
   customDateTimeFormat: {
     apiFormat: 'YYYY-MM-DD HH:mm:ss',
